fcaacba94c71efa0c8b97ab3732f8f0a2bccee25
Narek Geghamyan <12162743+narek-g@users.noreply.github.com>
2022-06-26 10:52:00 -0700

    Update chatgui.cpp

--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -118,8 +118,9 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     ////
 
     // create chat logic instance
-    //_chatLogic = new ChatLogic(); 
-    _chatLogic= std::make_unique<ChatLogic>();
+    // _chatLogic = new ChatLogic();  // old 
+    _chatLogic = std::make_unique<ChatLogic>(); 
+
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
 
@@ -135,7 +136,8 @@ ChatBotPanelDialog::~ChatBotPanelDialog()
     //// STUDENT CODE
     ////
 
-    //delete _chatLogic;
+    // delete _chatLogic;
+    std::cout << "ClatBotPanelDialog destructed./n"; 
 
     ////
     //// EOF STUDENT CODE
