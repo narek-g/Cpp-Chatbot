7845927d9d1dd1067d42f0ece10651b3a00a59d5
Narek Geghamyan <12162743+narek-g@users.noreply.github.com>
2022-06-26 11:18:04 -0700

    Update graphnode.cpp

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -1,7 +1,7 @@
 #include "graphedge.h"
 #include "graphnode.h"
-#include <iostream>
 #include "chatlogic.h"
+#include  <iostream>
 
 GraphNode::GraphNode(int id)
 {
@@ -13,8 +13,7 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    //delete _chatBot; 
-    std::cout << "GraphNode destructed" << std::endl;
+    // delete _chatBot; 
 
     ////
     //// EOF STUDENT CODE
@@ -39,15 +38,16 @@ void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 ////
 void GraphNode::MoveChatbotHere(ChatBot chatbot)
 {
+    // _chatBot = chatbot;
     _chatBot = std::move(chatbot);
     _chatBot.GetChatLogicHandle()->SetChatbotHandle(&_chatBot);
-    _chatBot.SetCurrentNode(this);
+    _chatBot->SetCurrentNode(this);
 }
 
 void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
 {
     newNode->MoveChatbotHere(std::move(_chatBot));
-    //_chatBot = nullptr; // invalidate pointer at source
+    // _chatBot = nullptr; // invalidate pointer at source
 }
 ////
 //// EOF STUDENT CODE
