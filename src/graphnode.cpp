d57a09a08e817e63e64929c77436d8eab05e06f9
Narek Geghamyan <12162743+narek-g@users.noreply.github.com>
2022-06-26 20:11:38 -0700

    Update graphnode.cpp

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -40,7 +40,7 @@ void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 void GraphNode::MoveChatbotHere(ChatBot chatbot)
 {
     _chatBot = std::move(chatbot);
-    // _chatBot.GetChatLogicHandle()->SetChatbotHandle(&_chatBot);
+    _chatBot.GetChatLogicHandle()->SetChatbotHandle(&_chatBot);
     _chatBot.SetCurrentNode(this);
 }
 
